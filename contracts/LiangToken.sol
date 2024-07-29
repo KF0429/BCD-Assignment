73cabf333482c901b73804427808590725e34510
KF0429 <kamfei1987@gmail.com>
2024-07-29 23:35:52 +0800

    Update LiangToken.sol

--- contracts/LiangToken.sol
+++ contracts/LiangToken.sol
@@ -4,8 +4,10 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract LiangToken is ERC20, Ownable {
-    constructor() ERC20("LiangToken", "LTK") Ownable(msg.sender) {
-        _mint(msg.sender, 100000 * 10 ** 18);
+contract CryptoLend is ERC20, Ownable {
+    constructor() ERC20("CryptoLend", "CLTK") Ownable(msg.sender) {
+        //10m amount of tokens to Make sure the number of tokens is sufficient to withstand the collateral of 10m ether
+        _mint(msg.sender, 10000000 * 10 ** 18);
+        
     }
 }
