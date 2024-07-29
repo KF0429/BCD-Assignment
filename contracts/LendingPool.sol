b819942f0453519a9fbd9b419f4ee4b9c94b83d8
KF0429 <kamfei1987@gmail.com>
2024-07-29 23:28:11 +0800

    descripton of the contract

---
+++ contracts/LendingPool.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.20;
+
+
+/**
+ * @title core contract of the system
+ * - User can:
+ *  # Supply
+ *  # Withdraw 
+ *  # Repay
+ */
+
+contract LendingPool{
+    
+}
---
+++ contracts/PoolStorage.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.20;
+
+/**
+ * @title Pool Storage
+ * -This contract:
+ *  #act as a Pool to gather all the ether received from LendingPool contract
+ *  #received and fallback fucntion in this contract
+ *  #send the ether to user from pool while user request loan (can say it as user want to borrow money)
+ *  #withdraw the ether (if user want to take out their Collateral)
+ */
+
+contract PoolStorage{
+
+}
