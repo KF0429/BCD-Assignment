4856b1ee148e0726e449ce757635916416c5168f
KF0429 <kamfei1987@gmail.com>
2024-08-02 01:56:37 +0800

    Update DataStuct.sol

--- contracts/DataStuct.sol
+++ contracts/DataStuct.sol
@@ -4,7 +4,13 @@ pragma solidity ^0.8.20;
 library DataStruct{
     struct LendingData{
         address User;
-        uint EtherBalance;
-        uint TokenReceived;
+        uint EtherBalance;// Collateral
+        uint TokenReceived;// token own **maybe can use to cal interest when withdraw
+    }
+
+    struct BorrowingData{
+        // make ot easy first
+        address User;
+        uint debtAmount;
     }
 }
