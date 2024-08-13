1e4f42b08e52a8df24515c5e1335297ffa3ac0e7
KF0429 <kamfei1987@gmail.com>
2024-08-13 18:06:32 +0800

    Update PoolStorage.sol

--- contracts/PoolStorage.sol
+++ contracts/PoolStorage.sol
@@ -1,8 +1,11 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.20;
+import {PoolStorage} from 'contracts/PoolStorage.sol';
+import {DataStruct} from 'contracts/DataStruct.sol';
 
 /**
  * @title Pool Storage
+ * @author Yau Kam Fei
  * -This contract:
  *  #act as a Pool to gather all the ether received from LendingPool contract
  *  #received and fallback fucntion in this contract
@@ -11,26 +14,44 @@ pragma solidity ^0.8.20;
  */
 
 contract PoolStorage{
-    uint256 public totalEther;
+    uint256 public totalEther; // Amount Etheruem stored in this Contract
+    LendingPool public lendingPool;
 
     receive() external payable{
         //receive ether from LendingPool and store to totalEther
         totalEther += msg.value;
     }
 
-    fallback() external payable{
-        totalEther += msg.value;
+    function getLendingPoolAddress(address _lendingPool) external{
+        lendingPool = LendingPool(_lendingPool);
     }
 
+    /**
+     * @notice send Ether to the address
+     * @dev Through sendEther function, allow 'LendingPool' Contract to call this function on withdraw and borrow request
+     * @param to Send to the address
+     * @param amount amoun to send
+     * @param isBorrow A boolean to identify the action (true = borrow, false = withdraw)
+     */
+    function sendEther(address payable to, uint256 amount,bool isBorrow) external{
+        DataStruct.LendingData memory userData = lendingPool.getLendingData(msg.sender);
+        DataStruct.BorrowingData memory borrowingData = lendingPool.getBorrowingData(msg.sender);
 
-    function sendEther(address payable to, uint256 amount) external payable{
-        //send ether to lending pool contract -- using on borrow 
-        totalEther -= amount;
-        to.transfer(amount);
-    }
+        if(isBorrow){//true = borrow
+            if (userData.user != address(0) && borrowingData.user == address(0)) {
+                require(amount <= lendingPool.getMaxBorrow(msg.sender), "Borrow amount exceeds 80% of collateral");
 
-    //ignore this function first
-    function withdraw() external payable{
-
+            } else if (userData.user != address(0) && borrowingData.user != address(0)) {
+                require(borrowingData.debtAmount + amount <= lendingPool.getMaxBorrow(msg.sender), "Borrow amount exceeds 80% of collateral");
+            }
+            totalEther -= amount;
+            to.transfer(amount);
+        }else{// false = withdraw
+            if (userData.user !=address(0)){
+                require(userData.etherBalance >= amount,"Insuficent balance to withdraw");
+            }
+            totalEther -= amount;
+            to.transfer(amount);
+        }
     }
 }
